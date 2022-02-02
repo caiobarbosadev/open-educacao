97c199960e664090ee0a25868cb4b0a4fc803824
Caio H. Barbosa Garcia <62814794+CaioHenrique04@users.noreply.github.com>
2022-02-02 00:53:36 -0300

    Add files via upload

--- views/send-course.php
+++ views/send-course.php
@@ -80,6 +80,14 @@ session_start();
             navUL.classList.toggle('show')
         });
     </script>
+    <!--- Secure Site Seal - DO NOT EDIT --->
+    <span id="ss_img_wrapper_115-55_image_en">
+        <a href="http://www.alphassl.com/ssl-certificates/wildcard-ssl.html" target="_blank" title="SSL Certificates">
+            <img alt="Wildcard SSL Certificates" border=0 id="ss_img" src="//seal.alphassl.com/SiteSeal/images/alpha_noscript_115-55_en.gif" title="SSL Certificate">
+        </a>
+    </span>
+    <script type="text/javascript" src="//seal.alphassl.com/SiteSeal/alpha_image_115-55_en.js"></script>
+    <!--- Secure Site Seal - DO NOT EDIT --->
 </body>
 
 </html>
--- views/student-area.php
+++ views/student-area.php
@@ -95,7 +95,7 @@ include('../models/checks_login.php');
             <a class="report-button" href="#">Reportar</a>
         </section>
     </main>
-    
+
     <?php include("../views/layouts/footer.php"); ?>
     <script>
         var qnt_result_pg = 5; //quantidade de registro por página
@@ -123,6 +123,14 @@ include('../models/checks_login.php');
         });
     </script>
     <script src="https://kit.fontawesome.com/11e70aec3a.js" crossorigin="anonymous"></script>
+    <!--- Secure Site Seal - DO NOT EDIT --->
+    <span id="ss_img_wrapper_115-55_image_en">
+        <a href="http://www.alphassl.com/ssl-certificates/wildcard-ssl.html" target="_blank" title="SSL Certificates">
+            <img alt="Wildcard SSL Certificates" border=0 id="ss_img" src="//seal.alphassl.com/SiteSeal/images/alpha_noscript_115-55_en.gif" title="SSL Certificate">
+        </a>
+    </span>
+    <script type="text/javascript" src="//seal.alphassl.com/SiteSeal/alpha_image_115-55_en.js"></script>
+    <!--- Secure Site Seal - DO NOT EDIT --->
 </body>
 
 </html>
